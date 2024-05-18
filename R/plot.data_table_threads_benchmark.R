7285a63fdc41241fa02e5da62d19e2b705c123e5
Anirban <bloodraven166@gmail.com>
2024-05-17 23:45:41 -0700

    Refactored to only have one call to geom_point() and geom_text() each; removed medianTime from closestPoints to match columns with maxSpeedup for the rbind and added another column to help distinguish when plotting.

--- R/plot.data_table_threads_benchmark.R
+++ R/plot.data_table_threads_benchmark.R
@@ -36,14 +36,32 @@ plot.data_table_threads_benchmark <- function(x, ...)
     .SD[which.max(speedup - suboptimalSubset$speedup)]
   }, by = expr]
 
+plot.data_table_threads_benchmark <- function(x, ...)
+{
+  dt <- as.data.table(x)
+  dt[, speedup := medianTime[threadCount == 1] / medianTime, by = expr]
+
+  maxSpeedup <- dt[, .(threadCount = threadCount[which.max(speedup)], speedup = max(speedup)), by = expr]
+  idealSpeedup <- seq(1, getDTthreads())
+  idealSpeedupData <- data.table(threadCount = 1:getDTthreads(), speedup = idealSpeedup)
+  subOptimalSpeedupData <- data.table(threadCount = seq(1, getDTthreads(), length.out = getDTthreads()), speedup = seq(1, getDTthreads()/2, length.out = getDTthreads()))
+
+  closestPoints <- dt[, {
+    suboptimalSubset <- subOptimalSpeedupData[threadCount %in% .SD$threadCount, on = .(threadCount)]
+    .SD[which.max(speedup - suboptimalSubset$speedup)]
+  }, by = expr]
+
+  closestPoints[, medianTime := NULL]
+  closestPoints[, type := "Recommended"]
+  maxSpeedup[, type := "Best performing"]
+  combinedData <- rbind(maxSpeedup, closestPoints)
+
   ggplot(dt, aes(x = threadCount, y = speedup)) +
     geom_line(aes(linetype = "Measured")) +
     geom_line(data = idealSpeedupData, aes(x = threadCount, y = speedup, linetype = "Ideal"), color = "black") +
     geom_line(data = subOptimalSpeedupData, aes(x = threadCount, y = speedup, linetype = "Sub-optimal"), color = "black") +
-    geom_point(data = closestPoints, aes(x = threadCount, y = speedup, shape = "Recommended"), color = "black", size = 2) +
-    geom_point(data = maxSpeedup, aes(x = threadCount, y = speedup, shape = "Best performing"), color = "red", size = 2) +
-    geom_text(data = closestPoints, aes(label = threadCount), vjust = -0.5, size = 4, na.rm = TRUE) +
-    geom_text(data = maxSpeedup, aes(label = threadCount), vjust = -0.5, size = 4, na.rm = TRUE) +
+    geom_point(data = combinedData, aes(x = threadCount, y = speedup, shape = type), color = ifelse(combinedData$type == "Recommended", "black", "red"), size = 2) +
+    geom_text(data = combinedData, aes(label = threadCount), vjust = -0.5, size = 4, na.rm = TRUE) +
     geom_ribbon(aes(ymin = speedup - 0.3, ymax = speedup + 0.3), alpha = 0.5) +
     facet_wrap(. ~ expr) +
     coord_equal() +
