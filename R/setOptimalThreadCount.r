00902457456e3a79cebd2cb69936b2d3fe92d382
Anirban <bloodraven166@gmail.com>
2024-06-12 17:43:45 -0700

    Refactored the function to include the new 'type' argument (derived the closest points logic for recommended speedup from my plot method), updated roxygen comments and messages, etc.

--- R/setOptimalThreadCount.r
+++ R/setOptimalThreadCount.r
@@ -1,12 +1,12 @@
-#' Function to set the optimal thread count for a specific \code{data.table} function
+#' Function to set the thread count for a specific \code{data.table} function
 #'
 #' @param benchmarkData A \code{data.table} of class \code{data_table_threads_benchmark} containing benchmarked timings with corresponding thread counts.
 #'
-#' @param functionName The name of the \code{data.table} function for which to set the optimal thread count.
+#' @param funcName The name of the \code{data.table} function for which to set the thread count.
 #'
 #' @return NULL.
 #'
-#' @details Sets the thread count to the optimal value (fastest median runtime) for the specified \code{data.table} function based on the results obtained from \code{findOptimalThreadCount()}.
+#' @details Sets the thread count to either the optimal (fastest median runtime) or recommended value (default) based on the chosen type argument for the specified \code{data.table} function based on the results obtained from \code{findOptimalThreadCount()}.
 #'
 #' @export
 #'
@@ -17,14 +17,37 @@
 #' # Finding the best performing thread count for each benchmarked data.table function with a data size of 10000000 rows and 10 columns:
 #' benchmarkData <- data.table.threads::findOptimalThreadCount(1e7, 10)
 #' # Setting the optimal thread count for the 'forder' function:
-#' setOptimalThreadCount(benchmarkData, "forder")
+#' setThreadCount(benchmarkData, "forder", "optimal")
+#' getDTthreads()
+#' # Setting the recommended thread count for the 'nafill' function:
+#' setThreadCount(benchmarkData, "nafill", "recommended")
+#' getDTthreads()
 #' }
 
-setOptimalThreadCount <- function(benchmarkData, functionName)
+setThreadCount <- function(benchmarkData, functionName, type = "recommended")
 {
-  fastestMedianTime <- benchmarkData[expr == functionName, .(median = min(median)), by = expr]
-  bestThreadCount <- benchmarkData[fastestMedianTime, on = .(expr, median), threadCount]
+  if(!"speedup" %in% colnames(benchmarkData))
+  {
+    benchmarkData[, speedup := median[threadCount == 1] / median, by = expr]
+  }
+
+  if(type == "optimal")
+  {
+    fastestMedianTime <- benchmarkData[expr == functionName, .(median = min(median))]
+    bestThreadCount <- benchmarkData[expr == functionName & median == fastestMedianTime$median, threadCount]
+  }
+  else if(type == "recommended")
+  {
+    recommendedSpeedupSubset <- benchmarkData[expr == functionName & type == "recommended"]
+    merged <- benchmarkData[expr == functionName][recommendedSpeedupSubset, on = .(threadCount), nomatch = 0L]
+    closestPoint <- benchmarkData[expr == functionName][which.max(speedup - merged$speedup)]
+    bestThreadCount <- closestPoint$threadCount
+  }
+  else
+  {
+    stop("Invalid type specified. (Please use 'recommended' or 'optimal')")
+  }
 
   setDTthreads(bestThreadCount)
-  cat(sprintf("The number of threads that data.table will use has been set to %d, the thread count that achieved the best runtime for data.table::%s().\n", bestThreadCount, functionName))
+  cat(sprintf("The number of threads that data.table will use has been set to %d, the %s thread count for data.table::%s() based on the performed benchmarks.\n", bestThreadCount, type, functionName))
 }
