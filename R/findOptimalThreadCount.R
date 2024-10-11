2529d7a0f2cb2d3f39ee7f3ce0dd0dfd63198648
Ani <bloodraven166@gmail.com>
2024-10-10 21:34:31 -0700

    Considering a list to return, refactored the logic to include the plot data (lines & points) computation from the plot method

--- R/findOptimalThreadCount.R
+++ R/findOptimalThreadCount.R
@@ -27,15 +27,48 @@
 findOptimalThreadCount <- function(rowCount, colCount, times = 10, verbose = FALSE)
 {
   setDTthreads(0)
-  maxThreads <- getDTthreads()
+  systemThreadCount <- getDTthreads()
   results <- list()
-
-  for (threadCount in 1:maxThreads) {
+  
+  for(threadCount in 1:systemThreadCount) 
+  {
     results[[threadCount]] <- runBenchmarks(rowCount, colCount, threadCount, times, verbose)
   }
-
+  
   results.dt <- rbindlist(results)
   seconds.dt <- results.dt[, .(threadCount, expr, min, max, median)]
+  functions <- unique(seconds.dt$expr)
   setattr(seconds.dt, "class", c("data_table_threads_benchmark", class(seconds.dt)))
-  seconds.dt
+  seconds.dt[, `:=`(speedup = median[threadCount == 1] / median, type = "Measured"), by = expr]
+  
+  speedupData <- data.table(
+    expr = rep(functions, each = systemThreadCount),
+    threadCount = rep(1:systemThreadCount, length(functions)),
+    speedup = rep(seq(1, systemThreadCount), length(functions)),
+    type = "Ideal"
+  )
+  
+  recommendedSpeedupData <- data.table(
+    threadCount = seq(1, systemThreadCount, length.out = systemThreadCount),
+    speedup = seq(1, systemThreadCount / 2, length.out = systemThreadCount),
+    type = "Recommended"
+  )
+  
+  maxSpeedup <- seconds.dt[, .(threadCount = threadCount[which.max(speedup)], speedup = max(speedup), type = "Ideal"), by = expr]
+  
+  closestPoints <- seconds.dt[, {
+    recommendedSubset <- recommendedSpeedupData[threadCount %in% .SD$threadCount]
+    .SD[.SD$speedup >= recommendedSubset$speedup][which.max(speedup)]
+  }, by = expr]
+  closestPoints[, type := "Recommended"]
+  
+  # Using fill = TRUE for missing columns minTime, maxTime, and median in speedupData and maxSpeedup:
+  combinedLineData <- rbind(speedupData, seconds.dt, fill = TRUE)
+  combinedPointData <- rbind(maxSpeedup, closestPoints, fill = TRUE)
+  
+  list(
+    benchmarkResults = seconds.dt,
+    combinedLineData = combinedLineData,
+    combinedPointData = combinedPointData
+  )
 }
